66261268bee6bf0ee3724542d8c869ed708b98c9
garaoi798 <unggoykiat10@gmail.com>
2025-01-27 00:57:56 +0800

    remove unnecessary pages

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useContext  } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/frontend_assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios'
 
 const LoginPopup = ({setShowLogin}) => {
+
+    const {url,setToken} = useContext(StoreContext);  
 
     const [currState, setCurrState] = useState('Login')
     const [data, setData] = useState({
@@ -18,13 +22,33 @@ const LoginPopup = ({setShowLogin}) => {
         setData(data=>({...data, [name]: value}))
     }
 
-    // useEffect(() => {
-    //     console.log(data) // This will log the data object whenever it changes
-    // }, [data])
+    const onLogin = async (e) => {
+        e.preventDefault()
+        let newUrl = url;
+        if(currState === 'Login'){
+            newUrl += '/api/user/login'
+        }
+        else{
+            newUrl += '/api/user/register'
+        }
+
+        const response = await axios.post(newUrl, data);
+            
+        if(response.data.success){
+            if(currState === 'Login'){
+                setToken(response.data.token);
+                localStorage.setItem('token', response.data.token);
+                setShowLogin(false);             
+            }
+            else{
+                alert(response.data.message)
+            }
+        }
+    }
 
   return (
     <div className='login-popup'>
-        <form  className="login-popup-container">
+        <form onSubmit={onLogin} className="login-popup-container">
             <div className="login-popup-title">
                 <h2>{currState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
@@ -42,7 +66,7 @@ const LoginPopup = ({setShowLogin}) => {
                 <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required/>
                 <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Your password' required />
             </div>
-            <button>{currState==='Sign Up'?'Create account':'Login'}</button>
+            <button type='submit'>{currState==='Sign Up'?'Create account':'Login'}</button>
             {currState === 'Sign Up'?<div className="login-popup-condition">
                 <input type="checkbox" required />
                 <p>By continuing, I agree to the terms of us & privacy policy.</p>
