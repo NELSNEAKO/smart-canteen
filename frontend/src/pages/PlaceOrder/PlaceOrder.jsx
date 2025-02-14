7b377e8efda66aa238887bc91afa5e37312a1db5
garaoi798 <unggoykiat10@gmail.com>
2025-02-15 03:21:12 +0800

    Update PlaceOrder
    if no token and no reservation cant proceed in payment session

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,4 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState, } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './PlaceOrder.css';
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
@@ -49,6 +51,18 @@ function PlaceOrder() {
       setLoading(false);
     }
   };
+
+    const navigate = useNavigate();
+    
+
+    useEffect(()=>{
+      if (!token){
+        navigate('/cart')
+      }
+      else if(getTotalCartAmount()==0){
+        navigate('/cart')
+      }
+    },[token])
 
   
 
