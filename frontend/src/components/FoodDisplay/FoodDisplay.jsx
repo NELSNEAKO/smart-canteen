b4472f1b7ce932251bd6345b14fe62ab3c5e4c6d
NELSNEAKO <unggoykiat10@gmail.com>
2025-02-28 03:25:21 +0800

    final hopefuly

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,6 @@ function FoodDisplay({availability}) {
 
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
               if(availability==='All' || availability===item.availability ){
