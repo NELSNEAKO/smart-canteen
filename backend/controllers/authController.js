1c1f00c08bbfe77ed3feff690f9c8d1cd0458062
NELSNEAKO <unggoykiat10@gmail.com>
2025-03-17 15:09:57 +0800

    fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,8 +37,8 @@ const studentRegister = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            // secure: process.env.NODE_ENV === "production",
-            secure: false,
+            secure: process.env.NODE_ENV === "production",
+            // secure: false,
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
@@ -88,8 +88,8 @@ const studentLogin = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            // secure: process.env.NODE_ENV === "production",
-            secure: false,
+            secure: process.env.NODE_ENV === "production",
+            // secure: false,
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
